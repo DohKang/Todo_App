58d34c7daf9e7c33c27156762500bca08adbe53a
Dohyun Kang <ncfdk23@gmail.com>
2023-05-24 21:55:12 -0700

    Add mark_as_done & mark_as_undone

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,22 @@
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Task
+
+
 def addTask(request):
     task = request.POST['task']
     Task.objects.create(task=task)
+    return redirect('home')
+
+
+def mark_as_done(request, id):
+    task = get_object_or_404(Task, pk=id)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def mark_as_undone(request, id):
+    task = get_object_or_404(Task, pk=id)
+    task.is_completed = False
+    task.save()
     return redirect('home')
